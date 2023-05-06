bafa19f48a12985e386744bf5cbc7f50b184a8d3
Hridoy <iamnirob334@gmail.com>
2023-05-07 01:23:53 +0600

    fixed button and input field responsive problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Conversation from "./pages/Conversation"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import "./app.css"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./app.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Conversation from "./pages/Conversation";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/conversation' element={<Conversation />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/conversation" element={<Conversation />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,13 +2,13 @@ import React, { useContext, useState } from "react";
 import { AiContext } from "../contexts/AiContext";
 
 const Form = () => {
-  const [inputText, setInputText] = useState("")
-  const {processRequest} = useContext(AiContext)
+  const [inputText, setInputText] = useState("");
+  const { processRequest } = useContext(AiContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    processRequest(inputText)
-    setInputText('')
+    processRequest(inputText);
+    setInputText("");
   };
 
   const handleInputChange = (event) => {
@@ -18,20 +18,18 @@ const Form = () => {
   return (
     <div className="chat-form">
       <form onSubmit={handleSubmit}>
-        <div className="d-flex justify-content-between form-content">
-          <div className="input-field">
-            <input
-              type="text"
-              value={inputText}
-              onChange={handleInputChange}
-              placeholder="ask me anything"
-            />
-          </div>
-          <div className="button-feild">
-            <button type="submit" className="common-btn submit-btn">
-              send
-            </button>
-          </div>
+        <div className="d-flex justify-content-between gap-1 gap-sm-2 gap-md-3">
+          <input
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="ask me anything"
+            style={{ flex: "1" }}
+            className="text-field"
+          />
+          <button type="submit" className="common-btn submit-btn">
+            send
+          </button>
         </div>
       </form>
     </div>
