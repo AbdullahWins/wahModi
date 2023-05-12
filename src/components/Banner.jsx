a567cc19e8039f72e2aa205da5bbc485a0e70e78
AbdullahWins <noyononline@gmail.com>
2023-05-12 17:36:51 +0600

    fixed logo and typos

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                 <div className="d-flex align-items-center gap-3 mb-2">
                     {/* texts  */}
                     <div style={{flex:1}}>
-                        <h1 className="title">I am Narandra Modi Bot for you!</h1>
+                        <h1 className="title">I am Narendra Modi Bot for you!</h1>
                     </div>
                     <div >
                         <Link to="/conversation" className="action-btn common-btn">Ask me anything</Link>
